eabf7df88d45797d6c4f3fdaac3f0a7a0251317a
michaelxie1 <michaelxie12345@gmail.com>
2023-02-06 15:39:22 -0500

    Added most of the basic features - everything is functional for the most part

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ function changeColor() {
     //var randomColor = Math.floor(Math.random()*16777215).toString(16); later implement random color
     
 	let color = document.body.style.color;
-	if (color === 'blue') {
-		document.body.style.color = 'red';
+	if (color === 'blue' || color === 'black') {
+		document.body.style.color = 'white';
 	} else {
-		document.body.style.color = 'blue';
+		document.body.style.color = 'black';
 	}
 }
