e5290ef3d3d4d415f3186c3f4fbe59b1242e033e
michaelxie1 <michaelxie12345@gmail.com>
2023-02-09 18:12:15 -0500

    Finalized most of the features within the interface

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 "use strict";
+
+
+
+
+
+
 function changeColor() {
-    //var randomColor = Math.floor(Math.random()*16777215).toString(16); later implement random color
     
 	let color = document.body.style.color;
 	if (color === 'blue' || color === 'black') {
@@ -8,4 +13,23 @@ function changeColor() {
 	} else {
 		document.body.style.color = 'black';
 	}
+
 }
+/*
+//var randomColor = Math.floor(Math.random()*16777215).toString(16); later implement random color
+	function random_bg_color() {
+		var x = Math.floor(Math.random() * 256);
+		var y = Math.floor(Math.random() * 256);
+		var z = Math.floor(Math.random() * 256);
+		var bgColor = "rgb(" + x + "," + y + "," + z + ")";
+		 console.log(bgColor);
+		return bgColor;
+		}
+	//var randomColor = Math.floor(Math.random()*16777215).toString(16);
+	/*
+	let color = document.body.style.color;
+	if(color === 'blue' || color === randomColor){
+		document.body.style.color = randomColor;}
+	else{
+	document.body.style.color = 'blue';
+}*/
